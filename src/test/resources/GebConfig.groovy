1ee18fca2786dd9a2bfef2fb30a440bce5566fd8
amarbabu@gmail.com <amarbabu@gmail.com>
2016-12-12 15:56:04 +0530

    Chrome Diver Support added

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -4,8 +4,18 @@
 	See: http://www.gebish.org/manual/current/configuration.html
 */
 
+/*
 import org.openqa.selenium.firefox.FirefoxDriver
 
 driver = { new FirefoxDriver() }
+
+baseUrl = "http://gebish.org"
+*/
+
+import org.openqa.selenium.chrome.ChromeDriver
+driver = {
+   System.setProperty('webdriver.chrome.driver', 'C:/Download/chromedriver.exe')
+   new ChromeDriver()
+}
 
 baseUrl = "http://gebish.org"
